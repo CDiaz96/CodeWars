84282499308e62ec4bf067c01e36ca4cccc233d2
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-08-11 23:20:32 -0400

    Code War 2

---
+++ codewars2.js
@@ -0,0 +1,11 @@
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// Solution:
+
+function even_or_odd(number) {
+ if (number % 2 == 0) {
+      return "Even";
+  } else {
+      return "Odd";
+  }
+}
