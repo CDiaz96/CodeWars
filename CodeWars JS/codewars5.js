0ebfbffd71f77cffe3a9864e66ebbd577fe6f6b0
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-08-14 12:53:59 -0400

    Challenge 5

---
+++ CodeWars JS/codewars5.js
@@ -0,0 +1,10 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+
+
+
+Solution:
+
+function sayHello(name) {
+  return 'Hello,'+ " "+ name;
+}
