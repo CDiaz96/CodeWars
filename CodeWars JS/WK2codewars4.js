eb81d33c786fb250bca28b018db963b0a514db12
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-08-20 19:39:10 -0400

    Create WK2codewars4.js

---
+++ CodeWars JS/WK2codewars4.js
@@ -0,0 +1,11 @@
+// Returning Strings
+//
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+//
+// Solution:
+
+function greet(name){
+  return "Hello, " + name + " how are you doing today?"
+}
