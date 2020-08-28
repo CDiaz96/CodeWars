6db893bbe432e390aa59c89a0b04d3e26c38fea5
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-08-27 20:42:24 -0400

    Create WK3codewars4.js

---
+++ CodeWars JS/WK3codewars4.js
@@ -0,0 +1,16 @@
+
+//Keep Hydrated
+
+
+// Nathan loves cycling.
+//
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+//
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+//
+// Solution:
+
+function litres(time) {
+
+  return Math.floor(time/2);
+}
