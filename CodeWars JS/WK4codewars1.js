8e580fd0530858a6aebadce67e31292f72449d37
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-08-31 23:17:42 -0500

    Create WK4codewars1.js

---
+++ CodeWars JS/WK4codewars1.js
@@ -0,0 +1,13 @@
+// Is n divisible by x and y?
+//
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+//
+// Solution:
+
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y == 0)
+    return true;
+else{
+  return false;
+}
+}
