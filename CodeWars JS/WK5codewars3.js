d7c721dc6cf357118a48c435c3fa4231310fa3fb
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-09-09 20:23:45 -0400

    Create WK5codewars3.js

---
+++ CodeWars JS/WK5codewars3.js
@@ -0,0 +1,15 @@
+// Array Appender
+//
+// Write a function called appendArrays that appends the items from array 2 onto array 1, returning the newly formed array.
+//
+// After using your appendArrays function, the result should be [a,b,c,1,2,3]
+//
+// Your function should also be able to handle nested arrays.
+//
+// Your solution should account for a situation for either the first or second inputs aren't actually arrays.
+//
+// Solution:
+
+function appendArrays (arr1, arr2) {
+   return arr1.concat(arr2);
+   }
