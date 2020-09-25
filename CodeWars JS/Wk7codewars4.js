9d10b354f7ca5fc55323bedf92cf01c573779267
CDiaz96 <63973811+CDiaz96@users.noreply.github.com>
2020-09-24 23:50:29 -0400

    Create Wk7codewars4.js

---
+++ CodeWars JS/Wk7codewars4.js
@@ -0,0 +1,12 @@
+// Find the capitals:
+//
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+var solution = [];
+	for(i=0; i<word.length; i++){
+		if(word[i] === word[i].toUpperCase()){
+			solution.push(i);
+		}
+	}
+	return solution;
+};
